8c849955216e4a4272983cb1a85b2eaf9c068953
Analuvr <99057214+Analuvr@users.noreply.github.com>
2025-04-09 17:24:41 -0600

    Add files via upload
    API REST para gestión de usuarios con Spring Boot

---
+++ pandevs/src/main/java/org/generation/pandevs/model/User.java
@@ -0,0 +1,90 @@
+package org.generation.pandevs.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity // Anotar esta clase  como una entidad
+@Table(name = "users") // Especificar información adicional que podemos manipular (nombre de la tabla, si no se establece nombre, toma el de la clase)
+public class User {
+
+    @Id // Anotación que establece la llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Estrategia ligada a ID para generar un valor (IDENTITY: características de autoincrementable, única y not null)
+    @Column(name = "id_user") // Será una columna de la tabla (si no se establecen nombres, toma los del atributo)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true) // El campo username tiene una longitud de 50 (VARCHAR(50)), no es nulo (Not Null) y es único (Unique)
+    private String username;
+
+    @Column(length = 70, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 120, nullable = false)
+    private String password;
+
+    // Constructor vacío para JPA
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password);
+    }
+
+
+}
